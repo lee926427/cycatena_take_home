99ac6428d5c984062f0165da33a54f56d824a700
recoil <leev126g8@gmail.com>
2024-04-13 05:53:37 +0800

    style: 💄 layout dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import "./assets/styles/index.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="grid grid-cols-9 grid-rows-12 gap-x-10 h-screen bg-stone-400 dark:bg-slate-900 p-6">
+      <header className="col-span-9"></header>
+      <main className="col-span-9 grid grid-cols-9 grid-rows-12 gap-x-10 row-span-11">
+        <section className="col-span-6 row-span-12 gap-y-4 grid grid-cols-6 gap-x-4">
+          <div className="col-span-2 row-span-2 bg-stone-400 dark:bg-slate-800">
+            <ErrorBoundary fallback={<div></div>}>
+              <Suspense fallback={<div>loading</div>}>
+                <div></div>
+              </Suspense>
+            </ErrorBoundary>
+          </div>
+          <div className="col-span-2 row-span-2 bg-stone-400 dark:bg-slate-800">
+            <ErrorBoundary fallback={<div></div>}>
+              <Suspense fallback={<div>loading</div>}>
+                <div></div>
+              </Suspense>
+            </ErrorBoundary>
+          </div>
+          <div className="col-span-2 row-span-2 bg-stone-400 dark:bg-slate-800">
+            <ErrorBoundary fallback={<div></div>}>
+              <Suspense fallback={<div>loading</div>}>
+                <div></div>
+              </Suspense>
+            </ErrorBoundary>
+          </div>
+          <div className="col-span-full row-span-10 bg-stone-400 dark:bg-slate-800">
+            <ErrorBoundary fallback={<div></div>}>
+              <Suspense fallback={<div>loading</div>}></Suspense>
+            </ErrorBoundary>
+          </div>
+        </section>
+        <section className="col-span-3 row-span-12 bg-stone-400 dark:bg-slate-800">
+          <ErrorBoundary fallback={<div></div>}>
+            <Suspense fallback={<div>loading</div>}></Suspense>
+          </ErrorBoundary>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
